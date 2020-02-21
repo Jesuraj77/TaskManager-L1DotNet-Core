4246d429322a0b2d6aebfcb72cd1b542e92459ce
Jesuraj77 <isysraju2001@gmail.com>
2020-02-21 19:22:57 +0530

    Added

---
+++ TaskManager.Models/Tasks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager.Models
+{
+  public  class Tasks
+    {
+        public  int TaskId { get; set; }
+        public int ParentTaskId { get; set; }
+        public string TaskName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Priority { get; set; }
+
+    }
+}
---
+++ TaskManagerServices/TaskService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManager.Models;
+namespace TaskManagerServices
+{
+   public class TaskService
+    {
+
+        public void AddTask(Tasks task)
+        {
+        }
+
+        public void EndTask(Tasks task)
+        {
+        }
+
+        public Tasks SearchTask(Tasks task)
+        {
+            Tasks tasks = new Tasks();
+            return tasks;
+        }
+
+
+        public Tasks UpdateTask(int TaskId)
+        {
+            Tasks task = new Tasks();
+            return task;
+        }
+
+
+        public List<Tasks> ViewTask()
+        {
+            List<Tasks> tasklist = new List<Tasks>();
+            return tasklist;
+        }
+
+
+        public Tasks GetTaskById(int TaskId)
+        {
+            Tasks task = new Tasks();
+            return task;
+        }
+
+    }
+}
